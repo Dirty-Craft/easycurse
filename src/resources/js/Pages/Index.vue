cb3cc134bddda64d0e04c07e456cd2768522bf05
Parsa Shahmaleki <parsampsh@gmail.com>
2026-01-17 12:26:31 +0330

    fix(landing): stats section: fixed number being cut

--- src/resources/js/Pages/Index.vue
+++ src/resources/js/Pages/Index.vue
@@ -670,10 +670,10 @@ onUnmounted(() => {
 
 const formatNumber = (num) => {
     if (num >= 1000000) {
-        return (num / 1000000).toFixed(1) + "M";
+        return Math.round(num / 1000000) + "M";
     }
     if (num >= 1000) {
-        return (num / 1000).toFixed(1) + "K";
+        return Math.round(num / 1000) + "K";
     }
     return num.toString();
 };
